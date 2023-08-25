8c2b045a0dab3105714e29a6648d342af362eb21
hailong <506698615@qq.com>
2023-08-25 17:02:51 +0800

    spring boot curd

--- hailong/src/main/java/com/hailong/www/model/User.java
+++ hailong/src/main/java/com/hailong/www/model/User.java
@@ -7,8 +7,10 @@ import lombok.Data;
 @Data
 public class User {
     @TableId(type = IdType.AUTO)/*主键自增*/
-    private Integer id;
-    private String name;
-    private Integer age;
-    private String email;
+    private Integer id;/*主键id*/
+    private String name;/*姓名*/
+    private Integer age;/*年龄*/
+    private String email;/*邮箱*/
+    private Integer created;/*创建时间*/
+    private Integer updated;/*更新时间*/
 }

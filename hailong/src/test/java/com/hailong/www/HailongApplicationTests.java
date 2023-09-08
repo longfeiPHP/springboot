8ffc22087ea52d32e100c2e36e5aa79aa778c1a7
hailong <506698615@qq.com>
2023-09-08 11:42:17 +0800

    spring boot

--- hailong/src/test/java/com/hailong/www/HailongApplicationTests.java
+++ hailong/src/test/java/com/hailong/www/HailongApplicationTests.java
@@ -1,13 +1,19 @@
 package com.hailong.www;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HailongApplicationTests {
 
+	@DisplayName("测试1")
 	@Test
 	void contextLoads() {
+//		System.out.println("test");
+		/*断言*/
+		Assertions.assertEquals(1,1);
 	}
 
 }
